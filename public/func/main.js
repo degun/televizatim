9b89627540f517403e1391e4e24d008738739737
Edlir Velaj <Edlir Velaj>
2017-07-01 18:28:47 +0200

    Useri zgjedh fjalën. zhduket vello nga useri dhe mund të vizatojë. të tjerëve u del vello e tejdukshme që mos të vizatojnë. fillon loja.

--- public/func/main.js
+++ public/func/main.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-	var vizatim = new DrawingBoard.Board('viz', {
+  var vizatim = new DrawingBoard.Board('viz', {
     size: 2,
     controls: [
       'Color',
@@ -11,14 +11,20 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   var mjeti = $('.drawing-board-control-drawingmode>button');
+  var mjetet = $('.drawing-board-controls');
+  var viz = $('#viz');
   var navi = $('.drawing-board-control-navigation>button:nth-child(1)');
   navi.removeClass('drawing-board-control-navigation-reset');
 
   var nofka;
   var chatMsgs = $('#chat ul');
   var chatMsg = $('#chat ul li');
+  var vello = $("<div id='vello'></div>");
+  var mesazhe = $("<div id='mesazhe'></div>");
+  vello.css({ position: 'absolute', height: viz.height() + 32 + 'px', width: '100%', backgroundColor: 'rgba(0,0,0,0)', top: 0, left: 0, zIndex: 120 });
+  mesazhe.css({ position: 'absolute', height: viz.height() + 32 + 'px', width: '100%', backgroundColor: 'rgba(0,0,0,0.6)', top: 0, left: 0, zIndex: 119, display: 'flex', justifyContent: 'center' });
 
-  var socket  = io.connect();
+  var socket = io.connect();
 
   var hidden, visibilityState, visibilityChange;
 
@@ -30,32 +36,71 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var document_hidden = document[hidden];
 
-  document.addEventListener(visibilityChange, function() {
-    if(document_hidden != document[hidden]) {
-      if(document[hidden]) {
+  document.addEventListener(visibilityChange, function () {
+    if (document_hidden != document[hidden]) {
+      if (document[hidden]) {
         // Document hidden
       } else {
-        socket.emit('imazhiTani');
+        if (nofka !== undefined) {
+          socket.emit('imazhiTani');
+        }
       }
       document_hidden = document[hidden];
     }
   });
-
-  
   // kur shtyp butonin hyr
-  $('.hyr').on('click', function(){
-      nofka = $('.nofka').val();
-      dhoma = $('.dhoma').val();
-      vizatim.ctx.fillStyle = '#fff';
-      vizatim.ctx.fillRect(0, 0, vizatim.ctx.canvas.width, vizatim.ctx.canvas.height);
-      vizatim.history.stack = [];
-      socket.emit('hyri', nofka, dhoma);
-      $('#hyrja').hide();
-      $('#loja').css({display: 'flex'});
+  $('.hyr').on('click', function () {
+    nofka = $('.nofka').val();
+    dhoma = $('.dhoma').val();
+    vizatim.ctx.fillStyle = '#fff';
+    vizatim.ctx.fillRect(0, 0, vizatim.ctx.canvas.width, vizatim.ctx.canvas.height);
+    vizatim.history.stack = [];
+    socket.emit('hyri', nofka, dhoma);
+    $('#hyrja').hide();
+    $('#loja').css({ display: 'flex' });
+  });
+
+  // pritet sa të mblidhen gjithë lojtarët
+  socket.on('please wait', function (sek) {
+    viz.append(mesazhe);
+    mesazhe.html('');
+    mesazhe.append('<h2 style="color: white; position: relative; top: 48%">Ju lutem prisni ' + sek + ' sekonda</h2>');
+  });
+
+  // zgjidhet lojtari aktual. pyeten të gjithë: a je ti? ai që është dërgon mesazh pozitiv
+  socket.on('are you the chosen', function (lojtar) {
+    if (nofka == lojtar.nofka) {
+      socket.emit('i zgjedhuri', lojtar.nofka);
+    }
+  });
+
+  // ti i zgjedhuri zgjidh fjalën
+  socket.on('choose word', function (f1, f2, f3) {
+    var fjalaZgjedhur;
+    mesazhe.html('');
+    mesazhe.append('<button value="' + f1 + '">' + f1 + '</button><button value="' + f2 + '">' + f2 + '</button><button value="' + f3 + '">' + f3 + '</button>');
+    $('button').on('click', function(){
+      fjalaZgjedhur = $(this).val();
+      mesazhe.slideUp();
+      socket.emit('zgjodha', fjalaZgjedhur);
+    });
+
+  });
+
+  // ju të tjerët prisni se po zgjedh ay
+  socket.on('is choosing', function (nofka) {
+    mesazhe.html('');
+    mesazhe.append('<h2 style="color: white; position: relative; top: 48%">' + nofka + ' po zgjedh fjalën</h2>');
+  });
+
+  // u zgjodh fjala - të gjithëve t'u hiqet perdja e zezë
+  socket.on('word chosen', function () {
+    mesazhe.slideUp();
+    viz.append(vello);
   });
 
   // merr nga serveri gjendjen aktuale, laps-gomë-kovë, ngjyrë dhe madhësi maje
-  socket.on('tools', function(madhesi, ngjyre, mjet){
+  socket.on('tools', function (madhesi, ngjyre, mjet) {
     vizatim.ctx.lineWidth = madhesi;
     vizatim.setColor(ngjyre);
     vizatim.ev.unbind('board:startDrawing', fillo);
@@ -64,31 +109,30 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // vendos listën e lojtarëve majtas tek #users
-  socket.on('updatePlayers', function(users){
+  socket.on('updatePlayers', function (users, piket, rangjet) {
     var listOfUsers = $('#users ul');
     listOfUsers.html('');
     for (var i = 0; i < users.length; i++) {
-        listOfUsers.append('<li class="usr">' +users[i] + '</li>');
+      listOfUsers.append('<li class="usr">' + users[i] + ' ' + piket[i] + ' ' + rangjet[i] + '</li>');
     }
   });
 
   // kur lojtari futet, të shohë ç'është bërë deri tani
-  socket.on('loadHistory', function(imazh){
-    if(imazh){
+  socket.on('loadHistory', function (imazh) {
+    if (imazh) {
       vizatim.setImg(imazh);
-      console.log(imazh);
     }
   });
 
   // shkruaj në chat që u fut filani
-  socket.on('in', function(user){
+  socket.on('in', function (user) {
     var chatMsgs = $('#chat ul');
-    chatMsgs.append('<li class="msgx" style="color: blue">'+ user +' hyri në lojë.');
+    chatMsgs.append('<li class="msgx" style="color: blue">' + user + ' hyri në lojë.');
   })
 
   // lajmës serverit: mesazh
-  $('.msg').keypress(function(e) {
-    if(e.which == 13) {
+  $('.msg').keypress(function (e) {
+    if (e.which == 13) {
       var message = $(this).val();
       $(this).val('');
       socket.emit('chat msg', nofka, message);
@@ -96,42 +140,42 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // përgjigje nga serveri: mesazh
-  socket.on('message', function(user, mesazh){
-    chatMsgs.append('<li class="msgx"><strong>'+ user +':</strong> ' +mesazh + '</li>');
-    chatMsgs.scrollTop(function() { return this.scrollHeight;});
+  socket.on('message', function (user, mesazh) {
+    chatMsgs.append('<li class="msgx"><strong>' + user + ':</strong> ' + mesazh + '</li>');
+    chatMsgs.scrollTop(function () { return this.scrollHeight; });
   });
 
   // mesazh nga serveri nga serveri: doli dikush
-  socket.on('out', function(user){
+  socket.on('out', function (user) {
     var chatMsgs = $('#chat ul');
-    chatMsgs.append('<li class="msgx" style="color: red"> '+ user +' doli nga loja.');
+    chatMsgs.append('<li class="msgx" style="color: red"> ' + user + ' doli nga loja.');
   });
 
   // lajmës serverit: fillo vizato
   vizatim.ev.bind('board:startDrawing', fillo);
 
   // funksion: fillo vizatimin
-  function fillo(){
+  function fillo() {
     socket.emit('filloVizatim', vizatim.coords);
-    if(vizatim.getMode() === 'filler'){
-        socket.emit('mbush', vizatim.coords.current);
-        vizatim.fill({coords: vizatim.coords.current});
+    if (vizatim.getMode() === 'filler') {
+      socket.emit('mbush', vizatim.coords.current);
+      vizatim.fill({ coords: vizatim.coords.current });
     };
   };
 
   // përgjigje nga serveri: fillo vizato
   socket.on('startLine', function (data) {
     vizatim.coords.current = vizatim.coords.old = vizatim.coords.oldMid = data.current;
-    if(vizatim.getMode() !== 'filler'){
-       vizatim.isDrawing = true;
-       if (!window.requestAnimationFrame) vizatim.draw();
+    if (vizatim.getMode() !== 'filler') {
+      vizatim.isDrawing = true;
+      if (!window.requestAnimationFrame) vizatim.draw();
     }
   });
 
   // lajmës serverit: jam duke vizatuar
-  vizatim.ev.bind('board:drawing', function(){
-    if(vizatim.isDrawing){
-       socket.emit('vizato', vizatim.coords);
+  vizatim.ev.bind('board:drawing', function () {
+    if (vizatim.isDrawing) {
+      socket.emit('vizato', vizatim.coords);
     }
   });
 
@@ -143,36 +187,35 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // lajmës serverit: ndalo së vizatuari
-  vizatim.ev.bind('board:stopDrawing', function(){
+  vizatim.ev.bind('board:stopDrawing', function () {
     socket.emit('ndaloVizatim', vizatim.coords);
     vizatim.saveHistory();
-    socket.emit('ruajHistorine', vizatim.history.stack[vizatim.history.stack.length-1]);
+    socket.emit('ruajHistorine', vizatim.history.stack[vizatim.history.stack.length - 1]);
   });
 
   // përgjigje nga serveri: ndalo së vizatuari
-  socket.on('stopLine', function(data){
+  socket.on('stopLine', function (data) {
     vizatim.coords.current = vizatim.coords.old = vizatim.coords.oldMid = data.old;
-    if (vizatim.isDrawing){
-       vizatim.isDrawing = false;
+    if (vizatim.isDrawing) {
+      vizatim.isDrawing = false;
     };
   });
 
   // lajmës serverit: hyri mouse
-  vizatim.ev.bind('board:mouseOver', function(){
-    if (vizatim.isDrawing){
+  vizatim.ev.bind('board:mouseOver', function () {
+    if (vizatim.isDrawing) {
       socket.emit('mouse brenda', vizatim.coords);
     }
   });
 
   // përgjigje nga serveri: hyri mouse, vazhdo vizato
-  socket.on('mouse in', function(data){
+  socket.on('mouse in', function (data) {
     vizatim.coords.current = vizatim.coords.old = vizatim.coords.oldMid = data.old;
     fillo();
   });
 
   // lajmës serverit: zgjidh mjet tjetër, laps, gomë ose kovë
-  vizatim.ev.bind('board:mode',  function(){
-    console.log('boardmode');
+  vizatim.ev.bind('board:mode', function () {
     vizatim.ev.unbind('board:startDrawing', fillo);
     socket.emit('ndryshoMjet', vizatim.getMode());
     vizatim.ev.bind('board:startDrawing', fillo);
@@ -185,20 +228,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // përgjigje nga serveri: mbush në filan koordinatë
   socket.on('fillen', function (data) {
-    if(vizatim.getMode() === 'filler'){
-       vizatim.fill({coords: data});
-       vizatim.saveHistory();
-       socket.emit('ruajHistorine', vizatim.history.stack[vizatim.history.stack.length-1]);
+    if (vizatim.getMode() === 'filler') {
+      vizatim.fill({ coords: data });
+      vizatim.saveHistory();
+      socket.emit('ruajHistorine', vizatim.history.stack[vizatim.history.stack.length - 1]);
     };
   });
 
   // lajmës serverit: fshi komplet
-  navi.on('click', function(){
+  navi.on('click', function () {
     vizatim.ctx.fillStyle = '#fff';
     vizatim.ctx.fillRect(0, 0, vizatim.ctx.canvas.width, vizatim.ctx.canvas.height);
     socket.emit('fshi');
     vizatim.saveHistory();
-    socket.emit('ruajHistorine', vizatim.history.stack[vizatim.history.stack.length-1]);
+    socket.emit('ruajHistorine', vizatim.history.stack[vizatim.history.stack.length - 1]);
   })
 
   // përgjigje nga serveri: fshi komplet
@@ -208,7 +251,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // lajmës serverit: zgjidh mjet tjetër, laps, gomë ose kovë
-  vizatim.ev.bind('color:changed', function(){
+  vizatim.ev.bind('color:changed', function () {
     socket.emit('ndryshoNgjyre', vizatim.color);
   });
 
@@ -218,7 +261,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
   // lajmës serverit: zgjidh mjet tjetër, laps, gomë ose kovë
-  vizatim.ev.bind('size:changed', function(){
+  vizatim.ev.bind('size:changed', function () {
     socket.emit('ndryshoMaje', vizatim.ctx.lineWidth);
   });
 
--- server.js
+++ server.js
@@ -6,114 +6,182 @@ var port = process.env.PORT || 4004
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-	res.sendFile( __dirname + '/views/index.html' );
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 var dhomat = {};
 
-io.on('connection', function(socket){
+var fjalet = ['farmaci', 'doktor', 'ilaç', 'ushtar', 'pirat', 'burg', 'biçikletë', 'libër', 'vesh', 'sy', 'kalorës', 'pushkë', 'çifte', 'top', 'shkencë', 'gjysh', 'fantazmë', 'ambulancë', 'pantallona', 'fabrikë', 'shef', 'oxhak', 'lot', 'qafë', 'rrotë', 'qen', 'mace', 'kalë', 'derr', 'gjel', 'hajdut', 'shkallë', 'fshat', 'shtizë', 'pistoletë', 'çerek', 'mish', 'lopë', 'dhi', 'dele', 'qepë', 'karotë', 'mollë', 'dardhë', 'fik', 'rrush', 'portokall', 'bonsai', 'byrek', 'banane', 'shtëpi', 'natë', 'ditë', 'diamant', 'rubin', 'mbret', 'mbretëreshë', 'princ', 'varr', 'berber', 'marangoz', 'hidraulik', 'elektricist', 'këpucë', 'çizme', 'bluzë', 'jastëk', 'maskë', 'përparëse', 'benz', 'makinë', 'kamion', 'triçikël', 'plazh', 'varkë', 'perëndim', 'peshk', 'peshkaqen', 'peshkatar', 'birrë', 'koktejl', 'kafe', 'kasolle', 'portë', 'kufi', 'çadër', 'manikyr', 'dorë', 'tatuazh', 'abazhur', 'llambë', 'biskotë', 'qumësht', 'lavatriçe', 'televizor', 'qeros', 'zile', 'valixhe', 'piano', 'kitarë', 'çifteli', 'fizarmonikë', 'mikrofon', 'bilbil', 'shah', 'bateri', 'violinë', 'pulovër', 'shapka', 'rrip', 'kapele', 'pazar', 'gomar', 'lulëkuqe', 'luledielli', 'luledele', 'tërfil', 'vrasës', 'batman', 'superman', 'spiderman', 'shalqi', 'pjeshkë', 'shtrigë', 'magjistar', 'dem', 'eksperiment', 'parfum', 'lupë', 'gërshet', 'gërshërë', 'kopsë', 'çakmak', 'alen', 'planet', 'yll', 'nishan', 'stacion', 'hëna', 'krëhër', 'dollap', 'muzikë', 'bisedë', 'para', 'vetulla', 'mustaqe', 'mustardë', 'xhep', 'xhuxh', 'peshore', 'luan', 'akrep', 'gjarpër', 'ciklop', 'binjakë', 'balonë', 'shpellë', 'pyll', 'gadishull', 'ishull', 'taksi', 'shi', 'vetëtimë', 'lugë', 'pirun', 'cfurk', 'rrudha', 'baluke', 'qofte', 'stol', 'karrige', 'kolltuk', 'krevat', 'trotuar', 'ëndërr', 'qyp', 'akullore', 'limonatë', 'pica', 'domate', 'spec', 'provim', 'Trump', 'dinamit', 'çantë', 'çokollatë', 'mur', 'kilometër', 'karburant', 'karkalec', 'sandale', 'laps', 'kamp', 'kek', 'tortë', 'gotë', 'verë', 'çekiç', 'pinca', 'kaçavidë', 'bukë', 'parking', 'midhje', 'oktapod', 'flori', 'universitet', 'projektor', 'rrugë', 'basketboll', 'futboll', 'tenis', 'golf', 'bilardo', 'volejboll', 'këngëtar', 'nuse', 'dhëndër', 'granatë', 'snajper', 'kthesë', 'pallat', 'shkollë', 'aeroplan', 'nëndetëse', 'helikopter', 'anije', 'bretkosë', 're', 'sharrë', 'trapan', 'dritare', 'derë', 'dorezë', 'poker'];
+
+io.on('connection', function (socket) {
 
   // sapo hyn një user, ruajmë nofkën dhe dhomën etj etj
-  socket.on('hyri', function(nofke, dhome){
+  socket.on('hyri', function (nofke, dhome) {
     socket.username = nofke;
     socket.room = dhome;
     socket.join(dhome);
 
-    if(socket.room in dhomat){
-      dhomat[socket.room].nofkat.push({emri: nofke, piket: 0, rangu: 1});
-    }else{
-      dhomat[socket.room] = { 
-                              nofkat: [], 
-                              curImg: '', 
-                              curMadhesi: '2', 
-                              curColor: '#000', 
-                              curMjet: 'pencil'
-                            };
-      dhomat[socket.room].nofkat.push({emri: nofke, piket: 0, rangu: 1});
+    if (socket.room in dhomat) {
+      dhomat[socket.room].lojtaret.push({ nofka: socket.username, pike: 0, rangu: 1 });
+    } else {
+      dhomat[socket.room] = {
+        lojtaret: [],
+        curImg: '',
+        curMadhesi: '2',
+        curColor: '#000',
+        curMjet: 'pencil',
+        curLojtar: '',
+        raunde: 3,
+        fjale: fjalet,
+        fjala: ''
+      };
+      dhomat[socket.room].lojtaret.push({ nofka: socket.username, pike: 0, rangu: 1 });
     }
-    console.log(nofke +' hyri në '+ dhome)
-    io.sockets.in(socket.room).emit('updatePlayers', dhomat[socket.room].nofkat.map(function(nofk){
-      return nofk.emri;
-    }));
+    console.log(nofke + ' hyri në ' + dhome);
+    var emrat = dhomat[socket.room].lojtaret.map(function (nofk) {
+      return nofk.nofka;
+    });
+
+    var piket = dhomat[socket.room].lojtaret.map(function (nofk) {
+      return nofk.pike;
+    });
+
+    var rangjet = dhomat[socket.room].lojtaret.map(function (nofk) {
+      return nofk.rangu;
+    });
+
+    io.sockets.in(socket.room).emit('updatePlayers', emrat, piket, rangjet);
     socket.broadcast.to(socket.room).emit('in', nofke);
+
     socket.emit('tools', dhomat[socket.room].curMadhesi, dhomat[socket.room].curColor, dhomat[socket.room].curMjet);
+
+    if (dhomat[socket.room].lojtaret.length !== 1) {
+      socket.emit('loadHistory', dhomat[socket.room].curImg);
+    }
+
+    if (dhomat[socket.room].lojtaret.length === 1) {
+      var sek = 10;
+      function prisni() {
+        io.sockets.in(socket.room).emit('please wait', sek);
+        sek--;
+        if (sek < 1) {
+          clearInterval(intervali);
+          dhomat[socket.room].curLojtar = dhomat[socket.room].lojtaret[dhomat[socket.room].lojtaret.length - 1];
+
+          io.sockets.in(socket.room).emit('are you the chosen', dhomat[socket.room].curLojtar);
+        }
+      }
+      var intervali = setInterval(prisni, 1000);
+    }
+  });
+
+  // i zgjedhuri dërgon mesazh
+  socket.on('i zgjedhuri', function (nofka) {
+
+    var i1 = Math.floor(Math.random() * dhomat[socket.room].fjale.length);
+    var fjala1 = dhomat[socket.room].fjale[i1];
+    dhomat[socket.room].fjale.splice(i1, 1);
+
+    var i2 = Math.floor(Math.random() * dhomat[socket.room].fjale.length);
+    var fjala2 = dhomat[socket.room].fjale[i2];
+    dhomat[socket.room].fjale.splice(i2, 1);
+
+    var i3 = Math.floor(Math.random() * dhomat[socket.room].fjale.length);
+    var fjala3 = dhomat[socket.room].fjale[i3];
+    dhomat[socket.room].fjale.splice(i3, 1);
+
+    socket.emit('choose word', fjala1, fjala2, fjala3);
+
+    dhomat[socket.room].lojtaret.curLojtar = nofka;
+
+    socket.broadcast.to(socket.room).emit('is choosing', nofka);
+  });
+
+  // vjen fjala e zgjedhur nga i zgjedhuri
+  socket.on('zgjodha', function(fjala){
+    dhomat[socket.room].fjala = fjala;
+    socket.broadcast.to(socket.room).emit('word chosen');
+  });
+
+  // dërgoji vetëm atij që e kërkoi
+  socket.on('imazhiTani', function () {
+    socket.broadcast.to(socket.room).emit('is choosing', dhomat[socket.room].lojtaret.curLojtar);
     socket.emit('loadHistory', dhomat[socket.room].curImg);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: fillo të vizatosh
-  socket.on('imazhiTani', function(){
-    socket.emit('loadHistory',  dhomat[socket.room].curImg);
-  });
-
-  // dërgoju gjithë të tjerëve përveç meje: fillo të vizatosh
-  socket.on('chat msg', function(user, msg){
+  socket.on('chat msg', function (user, msg) {
     io.in(socket.room).emit('message', user, msg);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: fillo të vizatosh
-  socket.on('filloVizatim', function(coords){
+  socket.on('filloVizatim', function (coords) {
     socket.broadcast.to(socket.room).emit('startLine', coords);
   });
-  
+
   // dërgoju gjithë të tjerëve përveç meje: vazhdo vizato
-  socket.on('vizato', function(coords){
+  socket.on('vizato', function (coords) {
     socket.broadcast.to(socket.room).emit('drawLine', coords);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: ndalo së vizatuari
-  socket.on('ndaloVizatim', function(coords){
+  socket.on('ndaloVizatim', function (coords) {
     socket.broadcast.to(socket.room).emit('stopLine', coords);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: ndalo së vizatuari
-  socket.on('mouse jashte', function(){
+  socket.on('mouse jashte', function () {
     socket.broadcast.to(socket.room).emit('mouse out');
   });
 
   // dërgoju gjithë të tjerëve përveç meje: vizato
-  socket.on('mouse brenda', function(coords){
+  socket.on('mouse brenda', function (coords) {
     socket.broadcast.to(socket.room).emit('mouse in', coords);
   });
 
-  socket.on('ruajHistorine', function(img){
+  socket.on('ruajHistorine', function (img) {
     dhomat[socket.room].curImg = img;
   });
 
   // dërgoju gjithë të tjerëve përveç meje: fillo të vizatosh
-  socket.on('ndryshoMjet', function(mode){
+  socket.on('ndryshoMjet', function (mode) {
     dhomat[socket.room].curMjet = mode;
     socket.broadcast.to(socket.room).emit('changeMode', mode);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: mbush në filan koordinatë
-  socket.on('mbush', function(data){
+  socket.on('mbush', function (data) {
     socket.broadcast.to(socket.room).emit('fillen', data);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: fshi komplet
-  socket.on('fshi', function(){
+  socket.on('fshi', function () {
     socket.broadcast.to(socket.room).emit('resetBoard');
   });
 
   // dërgoju gjithë të tjerëve përveç meje: fshi komplet
-  socket.on('ndryshoNgjyre', function(color){
+  socket.on('ndryshoNgjyre', function (color) {
     dhomat[socket.room].curColor = color;
     socket.broadcast.to(socket.room).emit('changeColor', color);
   });
 
   // dërgoju gjithë të tjerëve përveç meje: fshi komplet
-  socket.on('ndryshoMaje', function(size){
+  socket.on('ndryshoMaje', function (size) {
     dhomat[socket.room].curMadhesi = size;
     socket.broadcast.to(socket.room).emit('changeSize', size);
   });
 
-  socket.on('disconnect', function(){
-    var index = dhomat[socket.room].nofkat.indexOf(socket.username);
-    dhomat[socket.room].nofkat.splice(index, 1);
-    if(dhomat[socket.room].nofkat.length == 0){
+  socket.on('disconnect', function () {
+    if (dhomat[socket.room] !== undefined) {
+      var index = dhomat[socket.room].lojtaret.indexOf(socket.username);
+      dhomat[socket.room].lojtaret.splice(index, 1);
+    }
+
+    if (dhomat[socket.room].lojtaret.length === 0 && socket.room !== undefined) {
       delete dhomat[socket.room];
     }
+
     console.log(socket.username + ' doli nga ' + socket.room);
     socket.broadcast.to(socket.room).emit('out', socket.username);
     socket.broadcast.to(socket.room).emit('updatePlayers', dhomat[socket.room]);
@@ -121,7 +189,7 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(port, function(){
-	console.log('Hapëm portën ' + port);
+http.listen(port, function () {
+  console.log('Hapëm portën ' + port);
 });
 
